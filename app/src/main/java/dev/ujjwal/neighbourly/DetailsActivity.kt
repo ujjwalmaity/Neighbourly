c365d876df7c3a1fdfdaddc1693d710164ff0623
ujjwal <ujjwalmaity98@gmail.com>
2020-04-28 20:34:39 +0530

    Initial commit

---
+++ app/src/main/java/dev/ujjwal/neighbourly/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package dev.ujjwal.neighbourly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
