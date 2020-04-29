5d46ff36a39aba455e90d76b09a1cdd85cc93dfe
ujjwal <ujjwalmaity98@gmail.com>
2020-04-29 23:31:22 +0530

    Implement UI

--- app/src/main/java/dev/ujjwal/neighbourly/DetailsActivity.kt
+++ app/src/main/java/dev/ujjwal/neighbourly/DetailsActivity.kt
@@ -2,11 +2,45 @@ package dev.ujjwal.neighbourly
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import kotlinx.android.synthetic.main.layout_feed_details.*
 
 class DetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+
+        type_ans.setOnClickListener {
+            type_ans.visibility = View.GONE
+            type_et.visibility = View.VISIBLE
+            type_mic.visibility = View.VISIBLE
+
+            type_send.visibility = View.GONE
+            type_location.visibility = View.GONE
+            type_user.visibility = View.VISIBLE
+        }
+
+        type_et.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s?.length == 0) {
+                    type_send.visibility = View.GONE
+                    type_location.visibility = View.GONE
+                    type_user.visibility = View.VISIBLE
+                } else {
+                    type_send.visibility = View.VISIBLE
+                    type_location.visibility = View.VISIBLE
+                    type_user.visibility = View.GONE
+                }
+            }
+        })
     }
 }
--- app/src/main/java/dev/ujjwal/neighbourly/MainActivity.kt
+++ app/src/main/java/dev/ujjwal/neighbourly/MainActivity.kt
@@ -1,12 +1,38 @@
 package dev.ujjwal.neighbourly
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Dialog
+import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.layout_feed.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        iv_gps.setOnClickListener {
+            val dialog = Dialog(this)
+            val window: Window = dialog.window!!
+            val wmLp = window.attributes
+            wmLp.gravity = Gravity.TOP
+            wmLp.width = WindowManager.LayoutParams.MATCH_PARENT
+            wmLp.height = WindowManager.LayoutParams.WRAP_CONTENT
+            window.attributes = wmLp
+            dialog.setContentView(R.layout.layout_popup)
+            dialog.setCancelable(true)
+            dialog.setCanceledOnTouchOutside(true)
+            dialog.show()
+        }
+
+        card_view.setOnClickListener {
+            val i = Intent(this, DetailsActivity::class.java)
+            startActivity(i)
+        }
     }
 }
