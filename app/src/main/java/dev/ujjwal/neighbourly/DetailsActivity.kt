94a7f252db14d4c098a7967b2ed55a5789a9c34f
ujjwal <ujjwalmaity98@gmail.com>
2020-05-08 21:55:57 +0530

    Add animation

--- app/src/main/java/dev/ujjwal/neighbourly/DetailsActivity.kt
+++ app/src/main/java/dev/ujjwal/neighbourly/DetailsActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.animation.AnimationUtils
 import kotlinx.android.synthetic.main.layout_feed_details.*
 
 class DetailsActivity : AppCompatActivity() {
@@ -15,12 +16,11 @@ class DetailsActivity : AppCompatActivity() {
 
         type_ans.setOnClickListener {
             type_ans.visibility = View.GONE
+
+            type_user.visibility = View.VISIBLE
             type_et.visibility = View.VISIBLE
             type_mic.visibility = View.VISIBLE
-
             type_send.visibility = View.GONE
-            type_location.visibility = View.GONE
-            type_user.visibility = View.VISIBLE
         }
 
         type_et.addTextChangedListener(object : TextWatcher {
@@ -31,14 +31,20 @@ class DetailsActivity : AppCompatActivity() {
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (s?.length == 0) {
-                    type_send.visibility = View.GONE
-                    type_location.visibility = View.GONE
+                if (s?.trim()?.length == 0 && type_user.visibility == View.GONE) {
+                    type_user.animation = AnimationUtils.loadAnimation(applicationContext, R.anim.push_up_in)
                     type_user.visibility = View.VISIBLE
-                } else {
-                    type_send.visibility = View.VISIBLE
-                    type_location.visibility = View.VISIBLE
+                    type_et.animation = AnimationUtils.loadAnimation(applicationContext, R.anim.slide_in_left)
+                    type_mic.animation = AnimationUtils.loadAnimation(applicationContext, R.anim.slide_in_left)
+                    type_send.animation = AnimationUtils.loadAnimation(applicationContext, R.anim.push_up_out)
+                    type_send.visibility = View.GONE
+                } else if (type_user.visibility == View.VISIBLE) {
+                    type_user.animation = AnimationUtils.loadAnimation(applicationContext, R.anim.push_up_out)
                     type_user.visibility = View.GONE
+                    type_et.animation = AnimationUtils.loadAnimation(applicationContext, R.anim.slide_in_right)
+                    type_mic.animation = AnimationUtils.loadAnimation(applicationContext, R.anim.slide_in_right)
+                    type_send.animation = AnimationUtils.loadAnimation(applicationContext, R.anim.push_up_in)
+                    type_send.visibility = View.VISIBLE
                 }
             }
         })
